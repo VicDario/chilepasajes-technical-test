8d9e439d127ebf3b7c17b28c857418af0a53356e
VicDario <26585356+VicDario@users.noreply.github.com>
2025-02-16 02:44:06 -0300

    feat: add get donki instruments use case

---
+++ app/Domain/UseCases/GetDonkiInstrumentsUseCaseInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Domain\UseCases;
+
+interface GetDonkiInstrumentsUseCaseInterface
+{
+    public function execute(): array;
+}
---
+++ app/Infrastructure/UseCases/GetDonkiInstrumentsUseCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Infrastructure\UseCases;
+
+use App\Domain\Repositories\DonkiRepositoryInterface;
+use App\Domain\UseCases\GetDonkiInstrumentsUseCaseInterface;
+
+class GetDonkiInstrumentsUseCase implements GetDonkiInstrumentsUseCaseInterface
+{
+    public function __construct(private DonkiRepositoryInterface $donkiRepository)
+    {
+        $this->donkiRepository = $donkiRepository;
+    }
+
+    public function execute(): array
+    {
+        $measurements = ['CME', 'IPS', 'FLR', 'SEP', 'MPC', 'RBE'];
+        $instruments = [];
+
+        foreach ($measurements as $measurement) {
+            $instrumentsUsedInMeasurement = $this->donkiRepository->getInstrumentsFromMeasurement($measurement);
+            $instruments = array_merge($instruments, $instrumentsUsedInMeasurement);
+        }
+
+        return ['instruments' => array_values($instruments)];
+    }
+}
