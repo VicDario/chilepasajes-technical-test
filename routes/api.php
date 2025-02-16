a2ce6acba5e9056f8d1a289ffc2699955cba8315
VicDario <26585356+VicDario@users.noreply.github.com>
2025-02-16 12:39:46 -0300

    feat: move api routes to api file and add prefix

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Presentation\Http\Controllers\DonkiController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/instruments', [DonkiController::class, 'getInstrumentsFromMeasurements']);
+Route::get('/activity-ids', [DonkiController::class, 'getActivityIdsFromMeasurements']);
+Route::get('/instruments/percentage-usage', [DonkiController::class, 'getInstrumentsUsage']);
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
-use App\Presentation\Http\Controllers\DonkiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/instruments', [DonkiController::class, 'getInstrumentsFromMeasurements']);
-Route::get('/activity-ids', [DonkiController::class, 'getActivityIdsFromMeasurements']);
-Route::get('/instruments/percentage-usage', [DonkiController::class, 'getInstrumentsUsage']);
