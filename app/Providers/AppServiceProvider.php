dec4994713b4b904b9c9b88a201598686fdf82e8
VicDario <26585356+VicDario@users.noreply.github.com>
2025-02-16 02:44:17 -0300

    feat: register services

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,13 @@
 
 namespace App\Providers;
 
+use App\Config\EnvPlugin;
+use App\Domain\Repositories\DonkiRepositoryInterface;
+use App\Domain\UseCases\GetDonkiInstrumentsUseCaseInterface;
+use App\Infrastructure\Repositories\DonkiRepository;
+use App\Infrastructure\UseCases\GetDonkiInstrumentsUseCase;
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +18,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(EnvPlugin::class, EnvPlugin::class);
+        $this->app->bind(ClientInterface::class, Client::class);
+
+        $this->app->bind(GetDonkiInstrumentsUseCaseInterface::class, GetDonkiInstrumentsUseCase::class);
+        $this->app->bind(DonkiRepositoryInterface::class, DonkiRepository::class);
     }
 
     /**
